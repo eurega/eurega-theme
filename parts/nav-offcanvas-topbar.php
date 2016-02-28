62ba41e4d9b0b930e649e447c5317c28f615511b
Dominik Ritter <dritter03@googlemail.com>
2016-02-28 18:15:16 +0100

    Navigation angepasst (offcanvas)

--- parts/nav-offcanvas-topbar.php
+++ parts/nav-offcanvas-topbar.php
@@ -1,22 +1,13 @@
 <!-- By default, this menu will use off-canvas for small
 	 and a topbar for medium-up -->
 
-<div class="top-bar" id="top-bar-menu">
-	<div class="top-bar-left float-left">
-		<ul class="menu">
-      <li><a href="<?php echo home_url(); ?>">
-				<img src="<?php echo get_template_directory_uri(); ?>/assets/images/logo.png" alt="EUREGA-Logo" style="width: 20%;" />
-				<img src="<?php echo get_template_directory_uri(); ?>/assets/images/EUREGA-Schriftzug-blau.png" alt="EUREGA-Schriftzug" style="width: 43%;" />
-			</a></li>
-		</ul>
+<div class="title-bar" id="top-bar-menu">
+	<div class="title-bar-left show-for-medium">
+		<?php joints_top_nav(); ?>
 	</div>
-	<div class="top-bar-right show-for-medium">
-		<?php joints_top_nav(); ?>	
-	</div>
-	<div class="top-bar-right float-right show-for-small-only">
+	<div class="title-bar-left show-for-small-only">
 		<ul class="menu">
-			<!-- <li><button class="menu-icon" type="button" data-toggle="off-canvas"></button></li> -->
-			<li><a data-toggle="off-canvas"><?php _e( 'Menu', 'jointswp' ); ?></a></li>
+			 <li><button class="menu-icon" type="button" data-toggle="off-canvas"></button></li>
 		</ul>
 	</div>
 </div>
