382bed2d52786d9e45679f3530094d7cd8958467
Dominik Ritter <dritter03@googlemail.com>
2016-03-02 00:32:43 +0100

    Navigation auch auf kleinen Devices sticky

--- parts/nav-offcanvas-topbar.php
+++ parts/nav-offcanvas-topbar.php
@@ -1,7 +1,7 @@
 <!-- By default, this menu will use off-canvas for small
 	 and a topbar for medium-up -->
 <div data-sticky-container>
-	<div class="title-bar" id="top-bar-menu" style="width: 100%" data-sticky data-top-anchor="eurega-header:bottom" data-btm-anchor="content:bottom" data-options="marginTop:0;">
+	<div id="title-bar-menu" class="title-bar" style="width: 100%" data-sticky data-top-anchor="eurega-header:bottom" data-btm-anchor="content:bottom" data-options="marginTop:0;" data-sticky-on="small">
 		<div class="title-bar-left show-for-medium">
 			<?php joints_top_nav(); ?>
 		</div>
