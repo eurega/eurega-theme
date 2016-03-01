d9a3893243d1b6dbad402ec0c100a9fedebda24e
Dominik Ritter <dritter03@googlemail.com>
2016-03-01 19:01:16 +0100

    Burgermenu auf der rechten Seite anzeigen.

--- parts/nav-offcanvas-topbar.php
+++ parts/nav-offcanvas-topbar.php
@@ -2,10 +2,10 @@
 	 and a topbar for medium-up -->
 <div data-sticky-container>
 	<div class="title-bar" id="top-bar-menu" style="width: 100%" data-sticky data-top-anchor="eurega-header:bottom" data-btm-anchor="content:bottom" data-options="marginTop:0;">
-		<div class="title-bar-left show-for-medium">
+		<div class="title-bar-right show-for-medium">
 			<?php joints_top_nav(); ?>
 		</div>
-		<div class="title-bar-left show-for-small-only">
+		<div class="title-bar-right show-for-small-only">
 			<ul class="menu">
 				 <li><button class="menu-icon" type="button" data-toggle="off-canvas"></button></li>
 			</ul>
