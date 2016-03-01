8b8f01ad3f22c6e82d8c2f1f65efd2d92aca8699
Dominik Ritter <dritter03@googlemail.com>
2016-03-02 00:56:03 +0100

    Wenn die Navigation sticky ist, dann immer das Logo anzeigen

--- parts/nav-offcanvas-topbar.php
+++ parts/nav-offcanvas-topbar.php
@@ -2,13 +2,15 @@
 	 and a topbar for medium-up -->
 <div data-sticky-container>
 	<div id="title-bar-menu" class="title-bar" style="width: 100%" data-sticky data-top-anchor="eurega-header:bottom" data-btm-anchor="content:bottom" data-options="marginTop:0;" data-sticky-on="small">
-		<div class="title-bar-left show-for-medium">
-			<?php joints_top_nav(); ?>
-		</div>
-		<div class="title-bar-right show-for-small-only">
-			<ul class="menu">
-				 <li><button class="menu-icon" type="button" data-toggle="off-canvas"></button></li>
-			</ul>
+		<div class="title-bar--logo-container">
+			<div class="title-bar-left show-for-medium">
+				<?php joints_top_nav(); ?>
+			</div>
+			<div class="title-bar-right show-for-small-only">
+				<ul class="menu">
+					 <li><button class="menu-icon" type="button" data-toggle="off-canvas"></button></li>
+				</ul>
+			</div>
 		</div>
 	</div>
 </div>
