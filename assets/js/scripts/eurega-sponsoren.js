8bce5c2d63e8d6d3c23f926bee5f4778c8fe76cd
Dominik Ritter <dritter03@googlemail.com>
2016-03-04 00:03:55 +0100

    Sponsoren immer durchmischen

--- assets/js/scripts/eurega-sponsoren.js
+++ assets/js/scripts/eurega-sponsoren.js
@@ -27,21 +27,3 @@ jQuery(document).ready(function($) {
     // Sponsoren durchmischen.
     $('#sidebar1').find('.widget > h4:contains("Sponsoren")').parent().find('> p').shuffle();
 });
-jQuery(document).foundation();
-/* 
-These functions make sure WordPress 
-and Foundation play nice together.
-*/
-
-jQuery(document).ready(function() {
-    
-    // Remove empty P tags created by WP inside of Accordion and Orbit
-    jQuery('.accordion p:empty, .orbit p:empty').remove();
-    
-	 // Makes sure last grid item floats left
-	jQuery('.archive-grid .columns').last().addClass( 'end' );
-	
-	// Adds Flex Video to YouTube and Vimeo Embeds
-	jQuery('iframe[src*="youtube.com"], iframe[src*="vimeo.com"]').wrap("<div class='flex-video'/>");
-
-});
